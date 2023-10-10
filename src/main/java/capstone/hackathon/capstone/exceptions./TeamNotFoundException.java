190462beae33af7c19ec156bb0f78af8daf76751
sudhanshuchhaperwal00 <145001981+sudhanshuchhaperwal00@users.noreply.github.com>
2023-10-10 23:37:13 +0530

    Create TeamNotFoundException.java

---
+++ src/main/java/capstone/hackathon/capstone/exceptions./TeamNotFoundException.java
@@ -0,0 +1,7 @@
+package capstone.hackathon.capstone.exceptions;
+
+public class TeamNotFoundException extends RuntimeException{
+	public TeamNotFoundException(String message) {
+	super(message);
+	}
+}
