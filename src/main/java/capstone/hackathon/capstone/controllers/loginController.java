7771837bbdfa7bf3a347f75b2fd5c0e793e206c2
Dhruv Upadhyay <dhruv.upadhyay@incedoinc.com>
2023-10-12 16:08:40 +0530

    6th commit

--- src/main/java/capstone/hackathon/capstone/controllers/loginController.java
+++ src/main/java/capstone/hackathon/capstone/controllers/loginController.java
@@ -1,4 +1,5 @@
 package capstone.hackathon.capstone.controllers;
+import capstone.hackathon.capstone.web.dto.LoginResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
@@ -36,21 +37,19 @@ public class loginController {
 	//@PreAuthorize("hasAuthority('Role_User') or hasAuthority('Role_Admin') or hasAuthority('Role_Judge') or hasAuthority('Role_Panelist')")
 
 	@PostMapping("/login")
-
-	public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequest) {
+	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequestDto loginRequest) {
 		User user = userService.findByUsername(loginRequest.getUsername());
 		if (user != null && passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-
 			// Authentication successful, generate and return a token
 			String token = jwtService.generateToken(loginRequest.getUsername());
-			return ResponseEntity.ok(token);
-		}
-		else {
+			LoginResponse response = new LoginResponse(token, user.getRoles());
+			return ResponseEntity.ok(response); // Return the response entity with a 200 OK status
+		} else {
 			// Authentication failed
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); // Return 401 Unauthorized
 		}
-
 	}
+
 
 	@PostMapping("/resetPassword")
 	public ResponseEntity<String> resetPassword(@RequestBody ResetPasswordDto resetPasswordDto)
--- src/main/java/capstone/hackathon/capstone/web/dto/LoginResponse.java
+++ src/main/java/capstone/hackathon/capstone/web/dto/LoginResponse.java
@@ -1,4 +1,33 @@
 package capstone.hackathon.capstone.web.dto;
 
+import capstone.hackathon.capstone.entities.Role;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+
 public class LoginResponse {
+    private String token;
+    private List<Role> role;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public List<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(List<Role> role) {
+        this.role = role;
+    }
+
+    public LoginResponse(String token, List<Role> role) {
+        this.token = token;
+        this.role = role;
+    }
 }
