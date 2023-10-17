ad7cccc5a6e6f9f7fb1c275f840be7ae1a79b2a9
Dhruv Upadhyay <dhruv.upadhyay@incedoinc.com>
2023-10-17 12:01:18 +0530

    13th commit

--- src/main/java/capstone/hackathon/capstone/controllers/UserDashboardController.java
+++ src/main/java/capstone/hackathon/capstone/controllers/UserDashboardController.java
@@ -16,13 +16,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserDashboardController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private TeamRepository teamRepository;
@@ -107,6 +108,57 @@ public class UserDashboardController {
             }
         }
     }
+
+
+
+
+
+
+    @PreAuthorize("hasAuthority('Role_Panelist') or hasAuthority('Role_Leader') or hasAuthority('Role_User')")
+    @GetMapping("/dashboard/teamDetails")
+    public ResponseEntity<?> getTeamDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserInfoUserDetails user = (UserInfoUserDetails) authentication.getPrincipal();
+        Long userId = user.getId();
+
+        // Try to find the team where the user is the leader
+        Optional<Team> optionalTeam = teamRepository.findByLeaderId(userId);
+
+        if (optionalTeam.isPresent()) {
+            Team team = optionalTeam.get();
+
+            // Fetch the leader details from User entity
+            Optional<User> optionalLeader = userRepository.findById(team.getLeaderId());
+
+            if (optionalLeader.isPresent()) {
+                User leader = optionalLeader.get();
+
+                // Fetch the team members from team_members_table
+                List<TeamMembers> teamMembersList = teamMembersRepository.findByTeamId(team.getTeamId());
+                List<User> members = new ArrayList<>();
+
+                for (TeamMembers teamMembers : teamMembersList) {
+                    Optional<User> optionalUser = userRepository.findById(teamMembers.getMemberId());
+                    optionalUser.ifPresent(members::add);
+                }
+
+                // Create a map to hold the team details
+                Map<String, Object> teamDetails = new HashMap<>();
+                teamDetails.put("leader", leader);
+                teamDetails.put("members", members);
+
+                return ResponseEntity.ok(teamDetails);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Leader not found");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No Team Found");
+        }
+    }
+
+
+
+
 
 
 
--- src/main/java/capstone/hackathon/capstone/repository/TeamMembersRepository.java
+++ src/main/java/capstone/hackathon/capstone/repository/TeamMembersRepository.java
@@ -1,7 +1,9 @@
 package capstone.hackathon.capstone.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import capstone.hackathon.capstone.entities.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,13 @@ import capstone.hackathon.capstone.entities.TeamMembers;
 @Repository("TeamMembersRepository")
 public interface TeamMembersRepository extends JpaRepository<TeamMembers, Long>{
 	Optional<TeamMembers> findByMemberId(Long memberId);
+
+	//List<TeamMembers> findByLeaderIdOrMembersMemberId(Long leaderId, Long memberId);
+
+	List<TeamMembers> findByTeamId(Long teamId);
+
+
+
 
 	
 }
